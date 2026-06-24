7fc70b9b2f3025114baa761e508ddc0cc6e53d56
Marco Milanta <marco.milanta@gmail.com>
2026-06-24 16:50:45 +0200

    fix: minor

---
+++ math.md
@@ -0,0 +1,28 @@
+# product
+
+$$
+[abij,cdi\to abcd]\\
+X_{abij}Y_{cdi} \to Z_{abcd}.
+$$
+Now we ask
+$$
+Z_{abcd} =\sum_{ij}X_{abij}Y_{cdi},
+$$
+Now we ask
+$$
+\partial_{X_{1234}} Z_{abcd} = \partial_{X_{1234}}\sum_{ij}X_{abij}Y_{cdi} = Y_{cd3}, ab=12
+$$
+simpler case: inner
+$$
+[i,i\to], z= \sum_ix_iy_i
+$$
+so, 
+$$
+\nabla_x = y\nabla_z\\
+\nabla_x = [i,\to i](y, \nabla_z)
+$$
+outer
+$$
+[i,j\to ij], x_{i}y_{j} =z_{ij}.\\
+\nabla_x = [,ij,i\to i](\nabla _z,y,1)
+$$
--- src/gradient.py
+++ src/gradient.py
@@ -1,22 +1,29 @@
 from tensor import Tensor
 
+ALPHABET = "abcdefghijklmnopqrstuvxyz"
+
 class GTensor:
     def __init__(self, data: Tensor):
         self._data = data
         self._grad: list[Tensor] | None = None
-        self._grad_ref: list[GTensor] | None = None
+        self._partial_grad: list[Tensor] | None = None
+        self._partial_grad_operator: list[str] | None = None
+        self._references: list[GTensor] | None = None
 
     @classmethod
     def einsum(cls, einsum_command: str, *args: GTensor) -> GTensor:
-        
+        pass
 
     @classmethod
     def add(cls, *args: GTensor) -> GTensor:
         res = Tensor.add([arg._data for arg in args])
-        grad = Tensor([1 for _ in range(args[0]._data.size)])
+        partial_grad = Tensor([1 for _ in range(args[0]._data.size)], dims=res.dims)
         out = GTensor(
             data=res,
         )
-        out._grad=[grad for _ in range(len(args))]
-        out._grad_ref=list(args)
+        x = ALPHABET[:len(res.dims)]
+        out._partial_grad=[partial_grad for _ in range(len(args))]
+
+        out._partial_grad_operator=[f"{x},{x}->{x}" for _ in range(len(args))]
+        out._references=list(args)
 
